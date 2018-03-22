50997c90cff4ddade6d2b0292fb744d28c679ae9
jwwallin <jussi.w.wallin@gmail.com>
2018-03-22 20:30:47 +0200

    improve text

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,7 @@ class Header extends Component {
     return (
         <header className="App-header">
           <h1 className="App-title">Jussi Wallin</h1>
-          <h2 className="App-subtitle">Software developer, free-time electronics hacker and ICT student! (With a dab of gamer for flavor)</h2>
+          <h2 className="App-subtitle">Software developer, free-time electronics hacker and ICT student! (With a dash of gamer for flavor)</h2>
         </header>
     );
   }
