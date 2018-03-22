bf63e6d5b307483b1fac21fb7be0f656a89452a9
jwwallin <jussi.w.wallin@gmail.com>
2018-03-22 10:57:52 +0200

    move header to separate component, add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navigation from './Navigation.jsx';
+import Header from './Header.jsx';
 
 class App extends Component {
 
@@ -12,10 +13,12 @@ class App extends Component {
     return (
       <div className="App">
         <Navigation/>
-        <header className="App-header">
-          <h1 className="App-title">Jussi Wallin</h1>
-          <h2 className="App-subtitle">Software developer, free-time electronics hacker and ICT student! (With a dab of gamer for flavor)</h2>
-        </header>
+        <Header/>
+        <div className="page-footer">
+          Check me out on LinkedIn and GitHub:<br/>
+          <a href="https://www.linkedin.com/in/jussi-wallin-02267b91/"><img className="linkedin-logo" src="./In-2CRev-66px-TM.png" alt="LinkedIn"/></a>
+          <a href="https://github.com/jwwallin"><img className="github-logo" src="./GitHub-Mark-Light-64px.png" alt="GitHub"/></a>
+        </div>
       </div>
     );
   }
---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './Header.css';
+
+class Header extends Component {
+
+  render() {
+    return (
+        <header className="App-header">
+          <h1 className="App-title">Jussi Wallin</h1>
+          <h2 className="App-subtitle">Software developer, free-time electronics hacker and ICT student! (With a dab of gamer for flavor)</h2>
+        </header>
+    );
+  }
+}
+
+export default Header;
